198a45479160df5ff52b05c25a31211f765ef9c8
Елизавета Дегтярева <elizavetadegtareva@MacBook-Air-Elizaveta.local>
2021-11-23 23:20:41 +0300

    test edits

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -19,7 +19,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
 
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(expected,actual);
     }
     @Test
